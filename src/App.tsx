5c7c84a634522e055bed6c6b77e5adee43487a9f
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-16 18:06:38 -0400

    change of theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,225 +1,180 @@
 import { useState } from "react";
-import "./App.css";
+import { Button, Container, Grid, Paper, styled } from "@mui/material";
+import { GridDigitButton } from "./components/GridDigitButtons";
+import { GridOperationButton } from "./components/GridOperationButtons";
+
+const OutputContainer = styled(`div`)(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
 
 function App() {
-  const [answer, setAnswer] = useState("");
-  const [expression, setExpression] = useState("");
-  const et = expression.trim();
+  const [prevValue, setPrevValue] = useState("");
+  const [currentValue, setCurrentValue] = useState("0");
+  const [operation, setOperation] = useState("");
+  const [overwrite, setOverwrite] = useState(true);
 
-  const isOperator = (symbol: string) => {
-    return /[*/+-]/.test(symbol);
-  };
-
-  const buttonPress = (symbol: string) => {
-    if (symbol === "clear") {
-      setAnswer("");
-      setExpression("0");
-    } else if (symbol === "negative") {
-      if (answer === "") return;
-      setAnswer(
-        answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
-      );
-    } else if (symbol === "percent") {
-      if (answer === "") return;
-      setAnswer((parseFloat(answer) / 100).toString());
-    } else if (isOperator(symbol)) {
-      setExpression(et + " " + symbol + " ");
-    } else if (symbol === "=") {
-      calculate();
-    } else if (symbol === "0") {
-      if (expression.charAt(0) !== "0") {
-        setExpression(expression + symbol);
-      }
-    } else if (symbol === ".") {
-      // split by operators and get last number
-      const lastNumber = expression.split(/[-+/*]/g).pop();
-      if (!lastNumber) return;
-      console.log("lastNumber :>> ", lastNumber);
-      // if last number already has a decimal, don't add another
-      if (lastNumber?.includes(".")) return;
-      setExpression(expression + symbol);
-    } else {
-      if (expression.charAt(0) === "0") {
-        setExpression(expression.slice(1) + symbol);
-      } else {
-        setExpression(expression + symbol);
-      }
-    }
+  const equals = () => {
+    const val = calculate();
+    setCurrentValue(`${val}`);
+    setPrevValue("");
+    setOperation("");
+    setOverwrite(true);
   };
 
   const calculate = () => {
-    // if last char is an operator, do nothing
-    if (isOperator(et.charAt(et.length - 1))) return;
-    // clean the expression so that two operators in a row uses the last operator
-    // 5 * - + 5 = 10
-    const parts = et.split(" ");
-    const newParts = [];
+    if (!prevValue || !operation) return currentValue;
 
-    // go through parts backwards
-    for (let i = parts.length - 1; i >= 0; i--) {
-      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
-        newParts.unshift(parts[i]);
-        let j = 0;
-        let k = i - 1;
-        while (isOperator(parts[k])) {
-          k--;
-          j++;
-        }
-        i -= j;
-      } else {
-        newParts.unshift(parts[i]);
-      }
+    const curr = parseFloat(currentValue);
+    const prev = parseFloat(prevValue);
+
+    console.log("curr", curr);
+    console.log("prev", prev);
+    let result;
+    switch (operation) {
+      case "÷":
+        result = prev / curr;
+        break;
+      case "*":
+        result = prev * curr;
+        break;
+      case "-":
+        result = prev - curr;
+        break;
+      case "+":
+        result = prev + curr;
+        break;
     }
-    const newExpression = newParts.join(" ");
-    if (isOperator(newExpression.charAt(0))) {
-      setAnswer(eval(answer + newExpression) as string);
+    console.log("result", result);
+    return result;
+  };
+
+  const clear = () => {
+    setPrevValue("");
+    setOperation("");
+    setCurrentValue("0");
+    setOverwrite(true);
+  };
+
+  const del = () => {
+    setCurrentValue("0");
+    setOverwrite(true);
+  };
+
+  const percent = () => {
+    const curr = parseFloat(currentValue);
+    setCurrentValue((curr / 100).toString());
+  };
+
+  const selectOperation = (x: string) => {
+    if (prevValue) {
+      const val = calculate();
+      setCurrentValue(`${val}`);
+      setPrevValue(`${val}`);
     } else {
-      setAnswer(eval(newExpression) as string);
+      setPrevValue(currentValue);
     }
-    setExpression("");
+    setOperation(x);
+    setOverwrite(true);
+  };
+
+  const setDigit = (digit: string) => {
+    if (currentValue[0] === "0" && digit === "0") return;
+    if (currentValue.includes(".") && digit === ".") return;
+
+    if (overwrite && digit !== ".") {
+      setCurrentValue(digit);
+    } else {
+      setCurrentValue(`${currentValue}${digit}`);
+    }
+    setOverwrite(false);
   };
 
   return (
-    <>
-      <div className="container">
-        <h1>Calculator Application</h1>
-        <div id="calculator">
-          <div id="display" style={{ textAlign: "right" }}>
-            <div id="answer">{answer}</div>
-            <div id="expression">{expression}</div>
-          </div>
-          <button
-            id="clear"
-            onClick={() => buttonPress("clear")}
-            className="light-gray"
-          >
-            C
-          </button>
-          <button
-            id="negative"
-            onClick={() => buttonPress("negative")}
-            className="light-gray"
-          >
-            +/-
-          </button>
-          <button
-            id="percentage"
-            onClick={() => buttonPress("percentage")}
-            className="light-gray"
-          >
-            %
-          </button>
-          <button
-            id="divide"
-            onClick={() => buttonPress("/")}
-            className="yellow"
-          >
-            /
-          </button>
-          <button
-            id="seven"
-            onClick={() => buttonPress("7")}
-            className="dark-gray"
-          >
-            7
-          </button>
-          <button
-            id="eight"
-            onClick={() => buttonPress("8")}
-            className="dark-gray"
-          >
-            8
-          </button>
-          <button
-            id="nine"
-            onClick={() => buttonPress("9")}
-            className="dark-gray"
-          >
-            9
-          </button>
-          <button
-            id="multiply"
-            onClick={() => buttonPress("*")}
-            className="yellow"
-          >
-            *
-          </button>
-          <button
-            id="four"
-            onClick={() => buttonPress("4")}
-            className="dark-gray"
-          >
-            4
-          </button>
-          <button
-            id="five"
-            onClick={() => buttonPress("5")}
-            className="dark-gray"
-          >
-            5
-          </button>
-          <button
-            id="six"
-            onClick={() => buttonPress("6")}
-            className="dark-gray"
-          >
-            6
-          </button>
-          <button
-            id="subtract"
-            onClick={() => buttonPress("-")}
-            className="yellow"
-          >
-            -
-          </button>
-          <button
-            id="one"
-            onClick={() => buttonPress("1")}
-            className="dark-gray"
-          >
-            1
-          </button>
-          <button
-            id="two"
-            onClick={() => buttonPress("2")}
-            className="dark-gray"
-          >
-            2
-          </button>
-          <button
-            id="three"
-            onClick={() => buttonPress("3")}
-            className="dark-gray"
-          >
-            3
-          </button>
-          <button id="add" onClick={() => buttonPress("+")} className="yellow">
-            +
-          </button>
-          <button
-            id="zero"
-            onClick={() => buttonPress("0")}
-            className="dark-gray"
-          >
-            0
-          </button>
-          <button
-            id="decimal"
-            onClick={() => buttonPress(".")}
-            className="dark-gray"
-          >
-            .
-          </button>
-          <button
-            id="equals"
-            onClick={() => buttonPress("=")}
-            className="yellow"
-          >
-            =
-          </button>
-        </div>
-      </div>
-    </>
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <OutputContainer data-testid="output">
+              {currentValue}
+            </OutputContainer>
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridOperationButton
+              operation={"AC"}
+              selectOperation={clear}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"C"}
+              selectOperation={del}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"%"}
+              selectOperation={percent}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"÷"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"7"} enterDigit={setDigit} />
+            <GridDigitButton digit={"8"} enterDigit={setDigit} />
+            <GridDigitButton digit={"9"} enterDigit={setDigit} />
+            <GridOperationButton
+              operation={"*"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"4"} enterDigit={setDigit} />
+            <GridDigitButton digit={"5"} enterDigit={setDigit} />
+            <GridDigitButton digit={"6"} enterDigit={setDigit} />
+            <GridOperationButton
+              operation={"-"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"1"} enterDigit={setDigit} />
+            <GridDigitButton digit={"2"} enterDigit={setDigit} />
+            <GridDigitButton digit={"3"} enterDigit={setDigit} />
+
+            <GridOperationButton
+              operation={"+"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton xs={6} digit={"0"} enterDigit={setDigit} />
+            <GridDigitButton digit={"."} enterDigit={setDigit} />
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained" onClick={equals}>
+                =
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
+    </Container>
   );
 }
 
